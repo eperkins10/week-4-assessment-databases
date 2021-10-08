86c35260c5b2499201a1951727d932372c471efc
Ethan Perkins <ethanperkinspro@gmail.com>
2021-10-08 10:44:42 -0600

    finished sql extra credit

--- sql-skills.sql
+++ sql-skills.sql
@@ -45,6 +45,8 @@ WHERE invoice_id IN (
  WHERE unit_price > 0.99
  );
 
+
+
  SELECT i.invoice_date, c.first_name, c.last_name, i.total
 FROM invoice i 
 JOIN customer c 
@@ -59,6 +61,76 @@ SELECT a.title, ar.name
 FROM album a
 JOIN artist ar
 ON a.artist_id = ar.artist_id;
+
+-- /////EC SQL
+
+-- artist table
+
+SELECT * FROM artist
+ORDER BY name DESC LIMIT 10;
+
+SELECT * FROM artist
+WHERE name LIKE 'Black%';
+
+SELECT * FROM artist
+WHERE name LIKE '%Black%';
+
+-- employee table
+
+SELECT first_name, last_name, birth_date FROM employee
+ORDER BY birth_date DESC;
+
+SELECT birth_date FROM employee
+ORDER BY birth_date ASC;
+
+-- invoice table
+
+SELECT COUNT(*) FROM invoice
+WHERE billing_state IN ('CA', 'TX', 'AZ');
+
+SELECT AVG(total) FROM invoice;
+
+-- join query's
+
+SELECT t.name, p.name "Playlist Name"
+FROM track t
+JOIN playlist_track pt
+ON t.track_id = pt.track_id
+JOIN playlist p
+ON p.playlist_id = pt.playlist_id
+
+
+SELECT name FROM track 
+WHERE track_id IN (
+  SELECT track_id FROM playlist_track
+  WHERE playlist_id = 5 
+  );
+
+
+SELECT t.name, p.name "Playlist Name"
+FROM track t
+JOIN playlist_track pt
+ON t.track_id = pt.track_id
+JOIN playlist p
+ON p.playlist_id = pt.playlist_id
+
+
+SELECT t.name, a.title
+FROM track t
+JOIN genre g 
+ON g.genre_id = t.genre_id
+JOIN album a 
+ON t.album_id = a.album_id
+WHERE g.name = 'Alternative & Punk';
+​
+
+
+
+
+
+
+
+
 
 
 
