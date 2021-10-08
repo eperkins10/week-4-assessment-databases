3f01688ea6ff57a0700642de3d605af67b934bb5
Ethan Perkins <ethanperkinspro@gmail.com>
2021-10-08 09:39:33 -0600

    finished python section

--- process.py
+++ process.py
@@ -1,12 +1,16 @@
 log_file = open("um-server-01.txt")
+# opening csv file
 
-
+# start of function (def = function keyword)
 def sales_reports(log_file):
-    for line in log_file:
+    for line in log_file:   
         line = line.rstrip()
         day = line[0:3]
-        if day == "Tue":
+        if day == "Mon":
             print(line)
+
+# wrote a for loop, if the day in line index 0:3 was tuesday, it would print that line.
 
 
 sales_reports(log_file)
+# closing file/function
